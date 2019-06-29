2ef3085f5a546d8e27497a6eb2cdeeb531e70265
lilongwei <3394400272@qq.com>
2019-06-29 18:38:08 +0800

    3

--- src/main/java/com/aaa/sm/SpringBootMainApp.java
+++ src/main/java/com/aaa/sm/SpringBootMainApp.java
@@ -3,7 +3,7 @@ package com.aaa.sm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * className:SpringBootMainApp
@@ -11,8 +11,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * author:zz
  * createTime:2018-11-09 14:01
  */
-@EnableTransactionManagement //开启注解式事务
-@SpringBootApplication
+//@EnableTransactionManagement //开启注解式事务
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.aaa.sm.dao")
 public class SpringBootMainApp {
 
